211b098e9f3a510f537e53fbad394b16bc260b46
Rory Ashford <rory@superstrukt.co.uk>
2016-01-27 16:43:03 +0000

    Removed customisation options to keep it simple

--- maps.js
+++ maps.js
@@ -8,14 +8,6 @@ global.customGoogleMap = function () {
 
     var map,
         mapElement = document.getElementById('map_canvas'),
-        clrFillLabelText        = '#ee5065',
-        clrStrokeLabelText      = '',
-        clrFillHighway          = '#4b6578',
-        clrFillMediumRoad       = '#78a9cc',
-        clr_light_gray          = '#f3f5f5',
-        clrFillLocalRoad        = '#ffffff',
-        clrStrokeLocalRoad      = '#ccdae4',
-        clrFillWater            = '#79a8be',
         latLong = {
             lat: parseFloat(mapElement.getAttribute('data-latitude')), 
             lng: parseFloat(mapElement.getAttribute('data-longitude'))
@@ -29,66 +21,7 @@ global.customGoogleMap = function () {
         center: latLong,
         zoom: 13,
         disableDefaultUI: false,
-        scrollwheel:  false,
-        styles: [{
-            "featureType": "road.highway",
-            "elementType": "geometry.fill",
-            "stylers": [
-                { "color": clrFillHighway }
-            ]
-        },{
-            // Motorway stroke colour
-            "featureType": "road.highway",
-            "elementType": "geometry.stroke",
-            "stylers": [
-                { "visibility": "off" }
-            ]
-        },{
-            // Medium sized road colour
-            "featureType": "road.arterial",
-            "stylers": [
-                { "color": clrFillMediumRoad }
-            ]
-        },{
-            // Local road fill colour
-            "featureType": "road.local",
-            "elementType": "geometry.fill",
-            "stylers": [
-                { "color": clrFillLocalRoad }
-            ]
-        },{
-            // Local road stroke colour
-            "featureType": "road.local",
-            "elementType": "geometry.stroke",
-            "stylers": [
-                { "color": clrStrokeLocalRoad }
-            ]
-        },{
-            // Label text colour
-            "elementType": "labels.text.fill",
-            "stylers": [
-                { "color": clrFillLabelText }
-            ]
-        },{
-            // Label text stroke
-            "elementType": "labels.text.stroke",
-            "stylers": [
-                { "color": clrStrokeLabelText }
-            ]
-        },{
-            // Places of interest
-            "featureType": "poi",
-            "stylers": [
-                { "visibility": "off" }
-            ]
-        },{
-            // Water
-            "featureType": "water",
-            "elementType": "geometry",
-            "stylers": [
-                { "color": clrFillWater }
-            ]
-        }]
+        scrollwheel:  false
     });
 
     google.maps.event.addListener(map, 'click', function(event){
